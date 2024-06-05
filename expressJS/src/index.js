c27f972a692ddb7c7332c52b7636249fcea0766a
julio cesar <jcmcf2012@gmail.com>
2024-06-04 22:09:18 -0300

    feat: add expressjs full projects

---
+++ expressJS/src/index.js
@@ -0,0 +1,26 @@
+// Add your code here
+import express from "express";
+import cors from "cors";
+
+// If you want to connect to mongo uncomment the two lines below
+// and add the connection string to the .env file
+
+import { connectToMongo } from "./database/connect.js";
+connectToMongo();
+
+const app = express();
+app.use(
+  cors({
+    exposedHeaders: ["X-Total-Count"],
+  })
+);
+app.use(express.json());
+
+// SETUP ROUTES
+app.use("/", (req, res) => {
+  res.status(200).send({ working: true });
+});
+
+app.listen("3000");
+
+export default app;
