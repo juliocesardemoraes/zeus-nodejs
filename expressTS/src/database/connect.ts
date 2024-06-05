bbd78e5ae2e639271a315640a75dd0e6f1cdad93
julio cesar <jcmcf2012@gmail.com>
2024-06-04 22:21:32 -0300

    feat: add expressjs full projects

---
+++ expressTS/src/database/connect.ts
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+// Database connection URL
+const dbUrl = process.env.DB_KEY;
+
+// Connect to the database
+const connectToMongo = () => {
+  mongoose.connect(dbUrl);
+
+  mongoose.connection.on("connected", () => {
+    console.log("Connected to the database");
+  });
+
+  mongoose.connection.on("error", (err) => {
+    console.error("Database connection error:", err);
+  });
+
+  mongoose.connection.on("disconnected", () => {
+    console.log("Disconnected from the database");
+  });
+};
+
+export { connectToMongo };
